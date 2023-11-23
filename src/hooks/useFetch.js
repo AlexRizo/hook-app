a0e218f44ca981c3309a6057b0a2b2c0918b7981
Alex <david_alejandrorizo@outlook.com>
2023-11-23 15:19:53 -0600

    nextQuote pending

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,13 +1,36 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react";
 
 export const useFetch = (url) => {
-    useEffect(() => {
-        const getFetch = async () => {
-            
-        }
-    }, [url]);
+
+    const [state, setState] = useState({
+        data: null,
+        isLoading: true,
+        hasError: null,
+    });
     
-  return (
-    <div>useFetch</div>
-  )
-}
+    const getFetch = async () => {
+        setState({
+            ...state,
+            isLoading: true,
+        });
+        
+        const response = await fetch(url);
+        const data = await response.json();
+
+        setState({
+            data,
+            isLoading: false,
+            hasError: null,
+        });
+    };
+
+    useEffect(() => {
+        getFetch();
+    }, [url]);
+
+    return {
+        data: state.data,
+        isLoading: state.isLoading,
+        hasError: state.hasError,
+    };
+};
